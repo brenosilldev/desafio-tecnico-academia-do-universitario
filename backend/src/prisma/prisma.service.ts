a98648739bf1c99f61936c9b87f1c8f2b6e65877
Breno Silva <brenosill@hotmail.com>
2026-05-04 20:03:03 -0300

    feat; Criado módulo de prisma

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({ datasourceUrl: process.env.DATABASE_URL });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
