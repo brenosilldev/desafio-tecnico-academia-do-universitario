cc77cc271a850bd6372c8d71edc83c58433abf95
Breno Silva <brenosill@hotmail.com>
2026-05-04 22:32:31 -0300

    chore: Alterado props

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { TasksProvider } from '@/context/tasks-context'
+import { Providers } from '@/providers'
 import '@/styles/globals.css'
 
 export const metadata: Metadata = {
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-BR">
       <body>
-        <TasksProvider>{children}</TasksProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   )
